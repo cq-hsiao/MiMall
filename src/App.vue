07de85eb3712c62abe657ff71e792bb3122f6c9c
cq-hsiao <124328227@qq.com>
2020-08-18 16:15:09 +0800

    跨域的三种解决方式

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,32 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+//import axios from 'axios'
+import jsonp from 'jsonp'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      data:''
+    }
+  },
+
+  mounted() {
+    let url = '/apis/api/kefu/pc.php?game_id=2395'
+    // axios.get(url).then(()=>{
+
+    // })
+    jsonp(url,(err,res)=>{
+      let result = res;
+      this.data = result;
+    })
   }
 }
+
 </script>
 
 <style>
