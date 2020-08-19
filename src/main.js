73ad7ea3c8e8e44bfa40a093307b12c5ebe7c553
cq-hsiao <124328227@qq.com>
2020-08-19 17:12:35 +0800

    接口错误拦截配置、接口环境参数配置

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
-import App from './App.vue'
 import routers from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Env from './env'
 
-Vue.config.productionTip = false
+import App from './App.vue'
+
+// 根据前端的跨域方式做调整 /a/b : /api/a/b => /a/b
+// axios.defaults.baseURL = 'https://www.easy-mock.com/mock/5dc7afee2b69d9223b633cbb/mimall';
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000; 
+// 根据环境变量获取不同的请求地址
+// axios.defaults.baseURL = env.baseURL;
+// 接口错误拦截
+axios.interceptors.response.use(function(response){
+  let res = response.data;
+  if(res.status == 0) {
+    return res.data ;
+  } else if(res.status == 10) {
+    window.location.href = '/#/login/';
+  } else {
+    alert(res.msg);
+  }
+})
+
+Vue.use(VueAxios,axios)
+Vue.config.productionTip = false //生存环境提示
 
 new Vue({
   router:routers,
