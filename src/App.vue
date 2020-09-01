8f268417a65a9b70d22b19605c540105bdd1005c
cq-hsiao <124328227@qq.com>
2020-09-01 11:52:08 +0800

    NavHeader组件开发-1

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,6 @@
 
 <script>
 
-//import axios from 'axios'
-//import storage from './storage'
 
 export default {
   name: 'App',
@@ -21,28 +19,12 @@ export default {
   },
 
   mounted() {
-   // 1、本地加载请求静态json文件的形式
-  //  this.axios.get('/mock/user/login.json').then((res) => {
-  //    this.res = res;
-  //  })
-   // 2、通过easy-mock平台拉取模拟数据
-   // 3、通过mockjs插件模拟数据
-   this.axios.get('/user/login').then((res) => {
-     this.res = res;
-   })
-
+ 
   }
 }
 
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/scss/reset.scss';
 </style>
