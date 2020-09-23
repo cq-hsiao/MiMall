91bd7637af4e4609cc0dd801c811dffd056793f8
cq-hsiao <124328227@qq.com>
2020-09-23 12:02:14 +0800

    组件吸顶实现

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,11 +1,28 @@
 <template>
-    <div>
-      product
+    <div class="product">
+        <product-param>
+            <template v-slot:buy>
+                <button class="btn">立即购买</button>
+            </template>
+        </product-param>
+        <div class="h">123</div><div class="h">123</div><div class="h">123</div><div class="h">123</div><div class="h">123</div><div class="h">123</div>
     </div>
 </template>
 
 <script>
+    import ProductParam from './../components/ProductParam'
     export default {
         name:'product',
+        components:{
+            ProductParam
+        }
     }
 </script>
+
+<style lang="scss">
+    .product{
+        .h{
+            height: 150px;
+        }
+    }
+</style>
