03d9f023e4c426fc129638a1b61f897583c0585f
cq-hsiao <124328227@qq.com>
2020-11-23 17:24:53 +0800

    订单列表页面实现

--- src/main.js
+++ src/main.js
@@ -37,8 +37,8 @@ axios.interceptors.response.use(function(response){
     return Promise.reject(res);
   }
 },(error)=>{
-  let res = error.response;
-  Message.error(res.data.message);
+  // let res = error.response;
+  // Message.error(res.data.message);
   return Promise.reject(error);
 });
 
--- src/pages/orderList.vue
+++ src/pages/orderList.vue
@@ -2,46 +2,72 @@
     <div class="order-list">
         <order-header title="订单列表">
             <template v-slot:tip>
-                <span>请谨防钓鱼链接或诈骗电话，了解更多></span>
+                <span>订单列表页</span>
             </template>
         </order-header>
         <div class="wrapper">
             <div class="container">
                 <div class="order-box">
-                    <div class="order">
+                    <loading v-if="loading"></loading>
+                    <div class="order" v-for="(order,index) in list" :key="index">
                         <div class="order-title">
                             <div class="item-info fl">
-                                2019-12-20 11:43:08
+                                {{order.createTime}}
                                 <span>|</span>
-                                河畔一角
+                                {{order.receiverName}}
                                 <span>|</span>
-                                订单号：1352216743
+                                订单号：{{order.orderNo}}
                                 <span>|</span>
-                                在线支付
+                                {{order.paymentTypeDesc}}
                             </div>
                             <div class="item-money fr">
                                 <span>应付金额：</span>
-                                <span class="money">999</span>
+                                <span class="money">{{order.payment}}</span>
                                 <span>元</span>
                             </div>
                         </div>
                         <div class="order-content clearfix">
                             <div class="good-box fl">
-                                <div class="good-list">
+                                <div class="good-list" v-for="(item,i) in order.orderItemVoList" :key="i">
                                     <div class="good-img">
-                                        <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/4c87947d104ee5833913e4c520108f16.jpg" alt="">
+                                        <img v-lazy="item.productImage" v-bind:alt="item.productName">
                                     </div>
                                     <div class="good-name">
-                                        <div class="p-name">Redmi Note 8</div>
-                                        <div class="p-money">999 X 1元</div>
+                                        <div class="p-name">{{item.productName}}</div>
+                                        <div class="p-money">{{item.totalPrice + ' X ' + item.quantity}}元</div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="good-state fr">
-                                <a href="javascript:;">未支付</a>
+                            <div class="good-state fr" v-if="order.status == 20">
+                                <a href="javascript:;">{{order.statusDesc}}</a>
+                            </div>
+                            <div class="good-state fr" v-else>
+                                <a href="javascript:;" @click="goPay(order.orderNo)">{{order.statusDesc}}</a>
                             </div>
                         </div>
                     </div>
+                    <el-pagination
+                            v-if="false"
+                            class="pagination"
+                            background
+                            layout="prev, pager, next"
+                            :pageSize="pageSize"
+                            :total="total"
+                            @current-change="handleChange"
+                    >
+                    </el-pagination>
+                    <div class="load-more" v-if="false">
+                        <el-button type="primary" :loading="loading" @click="loadMore">加载更多</el-button>
+                    </div>
+                    <div class="scroll-more"
+                         v-infinite-scroll="scrollMore"
+                         infinite-scroll-disabled="true"
+                         infinite-scroll-distance="410"
+                         v-if="true"
+                    >
+                        <img src="/imgs/loading-svg/loading-spinning-bubbles.svg" alt="" v-show="loading">
+                    </div>
+                    <no-data v-if="!loading && list.length==0"></no-data>
                 </div>
             </div>
         </div>
@@ -50,10 +76,107 @@
 
 <script>
     import OrderHeader from './../components/OrderHeader';
+    import Loading from './../components/Loading'
+    import NoData from './../components/NoData'
+    import { Pagination,Button } from 'element-ui'
+    import infiniteScroll from 'vue-infinite-scroll'
     export default {
         name:'order-list',
         components:{
-            OrderHeader
+            OrderHeader,
+            Loading,
+            NoData,
+            [Pagination.name]:Pagination,
+            [Button.name]:Button
+        },
+        directives: {
+            infiniteScroll
+        },
+        data(){
+            return {
+                loading:false,
+                list: [],
+                pageSize:1,
+                pageNum:1,
+                total:0,
+                showNextPage:true,//加载更多：是否显示按钮
+                busy:false,//滚动加载，是否触发
+            }
+        },
+        mounted() {
+            this.getOrderList()
+        },
+        methods:{
+            getOrderList(){
+                this.loading = true;
+                this.busy = true;
+                this.axios.get('/orders',{
+                    params:{
+                        pageSize:1,
+                        pageNum:this.pageNum
+                    }
+                }).then((res) => {
+                    this.loading = false;
+                    this.list = this.list.concat(res.list);
+                    this.total = res.total;
+                    this.showNextPage = res.hasNextPage;
+                    this.busy = false;
+                }).catch(()=>{
+                    this.loading = false;
+                })
+            },
+            // 第一种方法：分页器
+            handleChange(pageNum){
+                this.pageNum = pageNum;
+                this.getOrderList();
+            },
+            // 第二种方法：加载更多按钮
+            loadMore(){
+                this.pageNum++;
+                this.getOrderList();
+            },
+            // 第三种方法：滚动加载，通过npm插件实现
+            scrollMore(){
+                this.busy = true;
+                setTimeout(()=>{
+                    this.pageNum++;
+                    this.getList();
+                },500);
+            },
+            // 专门给scrollMore使用
+            getList(){
+                this.loading = true;
+                this.axios.get('/orders',{
+                    params:{
+                        pageSize:1,
+                        pageNum:this.pageNum
+                    }
+                }).then((res)=>{
+                    this.list = this.list.concat(res.list);
+                    this.loading = false;
+                    if(res.hasNextPage){
+                        this.busy=false;
+                    }else{
+                        this.busy=true;
+                    }
+                });
+            },
+            goPay(orderNo){
+                // 三种路由跳转方式
+                // this.$router.push('/order/pay')
+                this.$router.push({
+                  name:'order-pay',
+                  query:{
+                    orderNo
+                  }
+                })
+                // this.$router.push({
+                //     path:'/order/pay',
+                //     query:{
+                //         orderNo
+                //     }
+                // })
+            },
         }
     }
 </script>
